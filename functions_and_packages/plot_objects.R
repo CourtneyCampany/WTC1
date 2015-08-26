730b85144c65a77ca09967850bb41f3f5e789e0e
Courtney Campany <courtneycampany@gmail.com>
2015-08-26 10:22:41 +1000

    plotted leaf area with treatments

--- functions_and_packages/plot_objects.R
+++ functions_and_packages/plot_objects.R
@@ -1,4 +1,9 @@
 library(scales)
 library(wesanderson)
 
-leaflab <- expression(Leaf~Area~~(m^-2))
+leaflab <- expression(Leaf~Area~~(m^2))
+
+
+ypos <- c(2.5,1,0)
+
+trtlab <-c(expression(aCO^2),expression(eCO^2), "Drought", "Control")
--- master_scripts/leafarea.R
+++ master_scripts/leafarea.R
@@ -1,3 +1,5 @@
+source("functions_and_packages/plot_objects.R")
+
 ##need to determine final leaves mass for correlations with GPP and TBCA
 
 leafarea <- read.csv("raw csv/HFE LA estimates alldates.csv")
@@ -10,8 +12,8 @@ chambersumm <- read.csv("raw csv/HFE chamber treatments.csv")
 
 leafarea <- merge(leafarea, chambersumm)
 
-
-order()
+##order by date
+leafarea <- leafarea[order(leafarea$Date),]
   
 ###plot bits
 startday <- as.Date(strptime("2008-04-15", format = "%Y-%m-%d", tz="UTC"))
@@ -29,12 +31,23 @@ cols <- c("black", "red")
 
 palette(c("black", "blue"))
 
+###plot leaf area----------------------------------------------------------------------------------------------------------
+
 windows(7,7)
-plot(LAestlin ~ Date, data=leafarea, type='n',ylab="Leaf Area",  xaxt='n', xlab="", xlim=xlimdays, ylim=c(0, 75))  
+
+par(mar=c(4,4,1,1), cex=1.25, las=1, cex.axis=.8, cex.lab=1, mgp=c(2.5,1,0))
+
+plot(LAestlin ~ Date, data=leafarea, type='n',ylab=leaflab,  xaxt='n', xlab="", xlim=xlimdays, ylim=c(0, 70))  
   axis.Date(1, at=xAT, labels=TRUE) #axis needs no labels
-  points(LAestlin ~ Date, data=leafarea, type='l', subset=chamber=="ch04", col="green")
+  #points(LAestlin ~ Date, data=leafarea, type='l', subset=chamber=="ch04", col="green")
   
   for (i in unique(leafarea$chamber)){
-    points(LAestlin ~ Date, data=leafarea[leafarea$chamber == i,], col=CO2_treatment)
+    points(LAestlin ~ Date, data=leafarea[leafarea$chamber == i,], col=CO2_treatment, type='l', lwd=2, lty=c(1,2)[Water_treatment])
   }
+  
+legend("topleft", trtlab, pch=c(15, 15, -1, -1), lty=c(-1, -1, 1,2), col=c("black", "blue", "black", "black"), bty='n', inset=0.01)
+  
+dev.copy2pdf(file="master_scripts/paper_figs/leafarea.pdf")
+dev.off() 
+  
 
