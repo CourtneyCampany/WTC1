d5e449534e25abc0318bcf6c3c23c694c7d00c33
Courtney Campany <courtneycampany@gmail.com>
2015-09-04 12:35:03 +1000

    tbca bar now with harvest carbon used in calc
    chamber_carbon.csv now has litterC

--- master_scripts/tbca_figure.R
+++ master_scripts/tbca_figure.R
@@ -1,43 +1,45 @@
 ### This script creates TBCA from the flux data and the biomass data
-source("functions_and_packages/functions.R")
-source("functions_and_packages/plot_objects.R")
-library(doBy)
+# source("functions_and_packages/functions.R")
+# source("functions_and_packages/plot_objects.R")
+# library(doBy)
 
 #1: Fc,t = GPP - (Rl + Rs,br) = total carbon flux by chamber
 #2: TBCA = Fc,t - Mab,t
 #3: Mab,t = total mass of aboveground + leaflitter
 
 # read harvest totals of mass and C flux by chmaber
-harvest_C <- read.csv("master_scripts/harvest_chamber.csv")
+#harvest_C <- read.csv("master_scripts/harvest_chamber.csv")
+tree_C <- read.csv("calculated_mass/chamber_carbon.csv")
+tree_C$treatment <- with(tree_C, paste(CO2_treatment, Water_treatment, sep="-"))  
 
 ##caluclate TBCA-----------------------------------------------------------------------------------------------------
 
-harvest_C$TBCA <- with(harvest_C, CO2cum - treeC)
+tree_C$TBCA <- with(tree_C, Cflux- treeC)
 
-harvest_C$Fs_resid <- with(harvest_C, TBCA - (frootC_all + CrootC))
+tree_C$Fs_resid <- with(tree_C, TBCA - (frootC + crootC))
 
 #write.csv(harvest_C[, c(1:2, 7:9, 15:16)], "calculated_mass/TBCA.csv", row.names = FALSE)
 
-#boxplot(TBCA ~treatment, data=harvest_C)
+boxplot(TBCA ~treatment, data=tree_C)
 
-below_means <- summaryBy(CO2cum+TBCA+Fs_resid ~ treatment, data=harvest_C, FUN=c(mean, se))
+below_means <- summaryBy(Cflux+TBCA+Fs_resid ~ treatment, data=tree_C, FUN=c(mean, se))
 
 
 ##simple plotting of tbca and residual ---------------------------------------------------------------------------------
 
-# bar(TBCA, c(treatment), harvest_C, col="grey", xlab="",legend=FALSE, ylim=c(0, 15000),
+# bar(TBCA, c(treatment), tree_C, col="grey", xlab="",legend=FALSE, ylim=c(0, 15000),
 #     half.errbar=FALSE, mar=c(5,5,1,1),las=1,cex.axis=.8, cex.lab=1, cex.names=1,mgp=c(3,1,0))
 # 
-# bar(Fs_resid, c(treatment), harvest_C, col="grey", xlab="",legend=FALSE, ylim=c(0, 15000),
+# bar(Fs_resid, c(treatment), tree_C, col="grey", xlab="",legend=FALSE, ylim=c(0, 15000),
 #     half.errbar=FALSE, mar=c(5,5,1,1),las=1,cex.axis=.8, cex.lab=1, cex.names=1,mgp=c(3,1,0))
 
 ###bar with both
 
 ##Create a two row matrix with tbca and Fs_resid
-below_dat <- rbind(below_means$CO2cum.mean,below_means$TBCA.mean, below_means$Fs_resid.mean)
+below_dat <- rbind(below_means$Cflux.mean,below_means$TBCA.mean, below_means$Fs_resid.mean)
 
-se_dat <- c(below_means$CO2cum.se,below_means$TBCA.se, below_means$Fs_resid.se)
-means_dat <-  c(below_means$CO2cum.mean,below_means$TBCA.mean, below_means$Fs_resid.mean)
+se_dat <- c(below_means$Cflux.se,below_means$TBCA.se, below_means$Fs_resid.se)
+means_dat <-  c(below_means$Cflux.mean,below_means$TBCA.mean, below_means$Fs_resid.mean)
 ##reorder
 se_dat2 <- se_dat[c(1,5,9,2,6,10,3,7,11,4,8,12)]
 means_dat2 <- means_dat[c(1,5,9,2,6,10,3,7,11,4,8,12)]
@@ -46,7 +48,7 @@ cols <- c("grey30", "grey55", "grey85")
 
 ###plot belowground flux
 
-windows(7,7)
+# windows(7,7)
 par(mar=c(5,6,1,1),las=1, mgp=c(3,1,0), cex.axis=1, cex.lab = 1.25)
 
 below_bar <- barplot(below_dat, beside=TRUE, names.arg=below_means$treatment, ylim=c(0, 27500), col=cols, 
@@ -58,8 +60,8 @@ legend("topright",belowfluxlab, pch = 22,  bty='n', pt.bg=cols, cex=1.25)
 axis(side=1, at=c(2.5, 6.5, 10.5, 14.5), labels=boxlab, padj=.75, cex.axis=1.25)
 title(ylab="Carbon  (g)", mgp=c(4,1,0))
 
-dev.copy2pdf(file="master_scripts/paper_figs/belowground_flux_plots.pdf")
-dev.off() 
+#  dev.copy2pdf(file="master_scripts/paper_figs/belowground_flux_plots.pdf")
+#  dev.off() 
 
 
   
